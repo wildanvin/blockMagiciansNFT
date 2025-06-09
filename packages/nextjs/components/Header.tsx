0074c13fac6afcfe57010975953f677670dd25a0
Wilman Vinueza <48974550+wildanvin@users.noreply.github.com>
2025-06-08 23:43:57 -0500

    Add View NFTs page and link

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -24,6 +24,10 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
   },
+  {
+    label: "View NFTs",
+    href: "/view-nfts",
+  },
 ];
 
 export const HeaderMenuLinks = () => {
